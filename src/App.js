7987d240181e15f4b8d62f221fa193f4e167e229
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-11-01 14:04:07 -0500

    Add card images, start header component; add deck data.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-    return (
-        <div>
-            Poker
-        </div>
-    );
+const App = () => {
+	return (
+		<React.Fragment>
+			<Header />
+		</React.Fragment>
+	);
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/_header.css';
+
+const Header = () => {
+	return (
+		<div className="header-container">
+			<h1>Raise or Fold?</h1>
+		</div>
+	);
+}
+
+export default Header;
